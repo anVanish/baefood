fe5270838b72a508e09af474564f015edd7e6cb4
Vanish <sosvanish@gmail.com>
2024-09-20 12:19:27 +0700

    add update food function backend added

--- backend/app/controllers/foodController.js
+++ backend/app/controllers/foodController.js
@@ -1,22 +1,22 @@
-const ApiError = require('../utils/ApiError')
-const ApiResponse = require('../utils/ApiResponse')
-const Foods = require('../models/Foods')
+const ApiError = require('../utils/ApiError');
+const ApiResponse = require('../utils/ApiResponse');
+const Foods = require('../models/Foods');
 
 // /foods/
 //GET /
 exports.listFood = async (req, res, next) => {
-    try{
-        const {categoryId} = req.query
-        const filter = {}
-        if (categoryId) filter.categoryId = categoryId
-        const page = req.query.page || 0
-        const limit = req.query.limit || 9
+    try {
+        const { categoryId } = req.query;
+        const filter = {};
+        if (categoryId) filter.categoryId = categoryId;
+        const page = req.query.page || 0;
+        const limit = req.query.limit || 9;
 
         const foods = await Foods.find(filter)
             .skip(page * limit)
-            .limit(limit)
-        
-        const total = await Foods.countDocuments(filter)
+            .limit(limit);
+
+        const total = await Foods.countDocuments(filter);
 
         res.json(
             new ApiResponse()
@@ -24,40 +24,51 @@ exports.listFood = async (req, res, next) => {
                 .setData('limit', limit)
                 .setData('page', page)
                 .setData('foods', foods)
-        )
-
-    }catch(error){
-        next(error)
+        );
+    } catch (error) {
+        next(error);
     }
-} 
+};
 
 //POST /
 exports.addFood = async (req, res, next) => {
-    try{
-
-    }catch(error){
-        next(error)
+    try {
+        const food = new Foods(req.body);
+        await food.save();
+        res.json(
+            new ApiResponse().setData('food', food).setSuccess('Food added')
+        );
+    } catch (error) {
+        next(error);
     }
-} 
+};
 
 //PUT /:foodId
 exports.updateFood = async (req, res, next) => {
-    try{
+    try {
+        const { foodId } = req.params;
+        const food = await Foods.findOneAndUpdate({ _id: foodId }, req.body, {
+            new: true,
+        });
+        if (!food) throw new ApiError('Food not found', 404);
 
-    }catch(error){
-        next(error)
+        res.json(
+            new ApiResponse().setSuccess('Food updated').setData('food', food)
+        );
+    } catch (error) {
+        next(error);
     }
-} 
+};
 
 //DELETE /:foodId
 exports.deleteFood = async (req, res, next) => {
-    try{
-        const {foodId} = req.params
-        const food = await Foods.findOneAndDelete({_id: foodId})
-        if (!food) throw new ApiError('Food not found', 404)
+    try {
+        const { foodId } = req.params;
+        const food = await Foods.findOneAndDelete({ _id: foodId });
+        if (!food) throw new ApiError('Food not found', 404);
 
-        res.json(new ApiResponse().setSuccess('Food deleted'))
-    }catch(error){
-        next(error)
+        res.json(new ApiResponse().setSuccess('Food deleted'));
+    } catch (error) {
+        next(error);
     }
-} 
+};
