1966f49fdca4a4135c3f9037a33f0df9585f8bc0
Vanish <sosvanish@gmail.com>
2024-09-20 20:08:48 +0700

    authorization wishlist page added

--- frontend/src/pages/Favorite.js
+++ frontend/src/pages/Favorite.js
@@ -1,37 +1,57 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import FoodCard from '../components/common/FoodCard';
+import { closeModal, openModal } from '../slices/loginModalSlice';
 
 const Favorite = () => {
+    const dispatch = useDispatch();
     const { wishlist } = useSelector((state) => state.wishlist);
+    const [user, setUser] = useState(null);
+
+    //check if user logged in
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser && !JSON.parse(storedUser).isAdmin) {
+            setUser(JSON.parse(storedUser));
+            dispatch(closeModal());
+        } else {
+            dispatch(openModal({ isAdmin: false }));
+        }
+    }, [dispatch]);
 
     return (
-        <section className="food_section layout_padding">
-            <div className="container">
-                <div className="heading_container heading_center">
-                    <h2>Món tủ của Bé Iu</h2>
-                </div>
+        <>
+            {!user ? (
+                <LoginModal />
+            ) : (
+                <section className="food_section layout_padding">
+                    <div className="container">
+                        <div className="heading_container heading_center">
+                            <h2>Món tủ của Bé Iu</h2>
+                        </div>
 
-                <div className="filters-content">
-                    <div className="row grid">
-                        {!wishlist ||
-                            (wishlist.length === 0 && (
-                                <div className="col-sm-12 col-lg-12">
-                                    <p className="text-center pt-4">
-                                        Chưa có món nào bé ơi!!
-                                    </p>
-                                </div>
-                            ))}
-                        {wishlist.map((food) => (
-                            <FoodCard
-                                key={food._id}
-                                food={food}
-                            />
-                        ))}
+                        <div className="filters-content">
+                            <div className="row grid">
+                                {!wishlist ||
+                                    (wishlist.length === 0 && (
+                                        <div className="col-sm-12 col-lg-12">
+                                            <p className="text-center pt-4">
+                                                Chưa có món nào bé ơi!!
+                                            </p>
+                                        </div>
+                                    ))}
+                                {wishlist.map((food) => (
+                                    <FoodCard
+                                        key={food._id}
+                                        food={food}
+                                    />
+                                ))}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </section>
+                </section>
+            )}
+        </>
     );
 };
 
