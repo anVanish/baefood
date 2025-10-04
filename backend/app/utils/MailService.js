cfff2ee79e4c525f892966028e60d75fd887faac
anVanish <sosvanish@gmail.com>
2025-10-04 23:13:18 +0700

    disable mail service

--- backend/app/utils/MailService.js
+++ backend/app/utils/MailService.js
@@ -14,11 +14,12 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.sendMail = async (to, subject, text) => {
-    const mailOptions = {
-        from: process.env.MAIL_USER,
-        to,
-        subject,
-        text,
-    };
-    return transporter.sendMail(mailOptions);
+    // const mailOptions = {
+    //     from: process.env.MAIL_USER,
+    //     to,
+    //     subject,
+    //     text,
+    // };
+    // return transporter.sendMail(mailOptions);
+    return null;
 };
